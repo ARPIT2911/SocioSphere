51f5b633710d840258eed29279208afbff48357b
Arpit <arpit2911011@gmail.com>
2023-07-21 21:45:27 +0530

    first commit

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+import Post from "../post/Post";
+import "./Profile.scss";
+import userImg from "../../assets/user.png";
+import { useNavigate, useParams } from "react-router";
+import CreatePost from "../createPost/CreatePost";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserProfile } from "../../redux/slices/postsSlice";
+import { followAndUnfollowUser } from "../../redux/slices/feedSlice";
+import { AiFillCloseCircle,AiOutlineUser} from "react-icons/ai";
+function Profile() {
+    const navigate = useNavigate();
+    const params = useParams();
+    const userProfile = useSelector((state) => state.postsReducer.userProfile);
+    const myProfile = useSelector((state) => state.appConfigReducer.myProfile);
+    const feedData = useSelector((state) => state.feedDataReducer.feedData);
+    const dispatch = useDispatch();
+    const [isMyProfile, setIsMyProfile] = useState(false);
+    const [isFollowing, setIsFollowing] = useState(false);
+
+    useEffect(() => {
+        dispatch(
+            getUserProfile({
+                userId: params.userId,
+            })
+        );
+        setIsMyProfile(myProfile?._id === params.userId);
+        setIsFollowing(
+            feedData?.followings?.find((item) => item._id === params.userId)
+        );
+    }, [myProfile, params.userId, feedData]);
+
+    function handleUserFollow() {
+        dispatch(followAndUnfollowUser({
+            userIdToFollow: params.userId
+        }))
+    }
+    const [isActive, setActive] = useState("false");
+
+    const ToggleClass = () => {
+      setActive(!isActive);
+    };
+    return (
+        <div className="Profile">
+            <div className="container">
+                <div className="left-part">
+                    {isMyProfile && <CreatePost />}
+                    {userProfile?.posts?.map((post) => (
+                        <Post key={post._id} post={post} />
+                    ))}
+                </div>
+                <div className={isActive ? "box" : "box-res"} onClick={ToggleClass}> <AiOutlineUser className="user"></AiOutlineUser> </div>
+                <div className={isActive ? "right-part" : "res-si"}>
+                    <div className="profile-card">
+                        <img
+                            className="user-img"
+                            src={userProfile?.avatar?.url ? userProfile?.avatar?.url : userImg}
+                            alt=""
+                        />
+                        <AiFillCloseCircle className='close' onClick={ToggleClass}></AiFillCloseCircle>
+                        <h3 className="user-name">{userProfile?.name}</h3>
+                        <p className="bio">{userProfile?.bio}</p>
+                        <div className="follower-info">
+                            <h4 className="text">{`${userProfile?.followers?.length} Followers`}</h4>
+                            <h4 className="text">{`${userProfile?.followings?.length} Followings`}</h4>
+                        </div>
+                        {!isMyProfile && (
+                            <h5
+                                style={{marginTop:'10px'}}
+                                onClick={handleUserFollow}
+                                className={
+                                    isFollowing
+                                        ? "hover-link follow-link"
+                                        : "btn-primary"
+                                }
+                            >
+                                {isFollowing ? "Unfollow" : "Follow"}
+                            </h5>
+                        )}
+                        {isMyProfile && (
+                            <button
+                                className="update-profile btn-secondary"
+                                onClick={() => {
+                                    navigate("/updateProfile");
+                                }}
+                            >
+                                Update Profile
+                            </button>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
---
+++ server/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const dbConnect = require("./dbConnect");
+const authRouter = require("./routers/authRouter");
+const postsRouter = require("./routers/postsRouter");
+const userRouter = require("./routers/userRouter");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
+
+dotenv.config("./.env");
+
+// Configuration
+cloudinary.config({
+  secure: true,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+
+//middlewares
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.send();
+});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "10mb" }));
+app.use(morgan("common"));
+app.use(cookieParser());
+let origin = "http://localhost:3000";
+console.log("here env", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  origin = process.env.CLIENT_ORIGIN;
+}
+
+app.use("/auth", authRouter);
+app.use("/posts", postsRouter);
+app.use("/user", userRouter);
+app.get("/", (req, res) => {
+  res.status(200).send("OK from Server");
+});
+
+const PORT = process.env.PORT || 4001;
+
+dbConnect();
+app.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`);
+});
+module.exports = app;
